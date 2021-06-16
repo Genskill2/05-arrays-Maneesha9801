c0695b21ac5ead9a8aec74b17fdc6dd6f87bb9c1
Maneesha a pillai <77294239+Maneesha9801@users.noreply.github.com>
2021-06-17 02:01:24 +0400

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,23 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max( int a[], int b){
+  int check =0;
+  for(int i=0;i<b;i++)
+  {
+    if(check<a[i]){
+      check=a[i];
+      
+    }
+    
+  }
+}
+int min ( int c[], int d){
+  int check1=0;
+  for(int i=0;i<d;i++){
+    if(check1>c[i]){
+      check1= c[i];
+    }
+  }
+}
+
+  
